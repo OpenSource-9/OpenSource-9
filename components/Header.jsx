545b5d4d13240e6652937e3d7a7b9262fc43a38d
fghy788 <fghy788@gmail.com>
2024-06-11 17:03:06 +0900

    Front : 상단에 오픈소스-9 주석처리 제거후 title을 업데이트
    상단 왼쪽 헤더에 오픈소스 -9 주석처리를 제거하고 SEO를 업데이트 했습니다.

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export const Header=({setMenu})=>{
   return <>
   <div className="flex justify-between border-b-4 border-black bg-red-700">
     <div className={`flex items-center m-4`}>
-      {/* <span className={`font-PixelBold text-xl`}>OpenSource 9</span> */}
+      <span className={`font-PixelBold text-xl`}>OpenSource 9</span>
     </div>
     <div className={`flex gap-5 m-4 font-PixelBold text-xl`}>
       <button  className={`hover:scale-110`} onClick={()=>setMenu(0)}>GAME</button>
